fabbcf9643a1a8e556089fc0a1c23eaa53017ce7
josephstakelan <josephstakeland@gmail.com>
2024-03-10 23:55:38 -0500

    Addaded Routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  //routes: [{ path: "/product-page/:slug", component: "./src/pages/product-page.astro" }],
   integrations: [tailwind()],
   output: "server",
 });
