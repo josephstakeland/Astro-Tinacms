15860a777a61e282cb46e7897959aa9b82f860bd
josephstakeland <josephstakeland@gmail.com>
2024-03-11 13:08:01 -0500

    Storing APIs

---
+++ public/APIwp.js
@@ -0,0 +1,2 @@
+export const API_REQ_1='http://astro-wp-headless.local/wp-json/wp/v2/streaming?acf_format=standard&_fields=id,title,slug,acf'
+export const API_REQ_2='http://astros-worpress-headless.local/wp-json/wp/v2/streaming?acf_format=standard&_fields=id,title,slug,acf'
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="astro/client" />
+
+API_REQ_1='http://astro-wp-headless.local/wp-json/wp/v2/streaming?acf_format=standard&_fields=id,title,slug,acf'
+API_REQ_2='http://astros-worpress-headless.local/wp-json/wp/v2/streaming?acf_format=standard&_fields=id,title,slug,acf'
